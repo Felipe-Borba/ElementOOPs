0c6a522cda89cbf3a3e05345838e34480ed3277e
Felipe Borba <felipe.borba0109@gmail.com>
2023-04-29 08:57:14 -0300

    refactor menu

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Menu menu = new Menu();
+        Menu.novoJogo();
 
-        Menu.Opcoes opcao = menu.showMenuPrincipal();
-
-        if (opcao == Menu.Opcoes.NOVO_JOGO) {
-            System.out.println("todo novo jogo");
-        }
+        System.out.println("todo novo jogo");
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -3,37 +3,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Menu {
-    public enum Opcoes {
-        NOVO_JOGO
-    }
-
-    private void sair() {
+    private static void sair() {
         System.out.println("Tchau!");
         System.exit(0);
     }
 
-    private int readIntegerLine() throws IOException {
+    private static int readIntegerLine() throws IOException {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         return Integer.parseInt(buffer.readLine());
     }
 
-    public Opcoes showMenuPrincipal() throws IOException {
-        System.out.println("Escolha uma opção: \n 0 - sair \n 1 - novo jogo");
+    public static boolean novoJogo() throws IOException {
+        System.out.println(
+                "Escolha uma opção: \n"
+                        + "0 - sair \n"
+                        + "1 - novo jogo"
+        );
 
         try {
-            int i = this.readIntegerLine();
+            int i = Menu.readIntegerLine();
 
             if (i == 0) {
-                this.sair();
+                Menu.sair();
             } else if (i == 1) {
-                return Opcoes.NOVO_JOGO;
+                return true;
             } else {
                 throw new NumberFormatException();
             }
         } catch (NumberFormatException nfe) {
             System.out.println("Opção inválida!\n");
-            return this.showMenuPrincipal();
+            return Menu.novoJogo();
         }
-        throw new Error("Ops, algo de errado não está certo");
+        return false;
+    }
+
+    public static Criatura selecionaCriatura() throws Exception {
+        System.out.println(
+                "Escolha uma criatura: \n"
+                        + "0 - sair\n"
+                        + "1 - StoneDev (terra)\n"//get from typeof
+                        + "2 - WaveNerd (água)\n"//get from typeof
+                        + "3 - BurnCoder (fogo)"//get from typeof
+                        + "4 - BreezeHacker (ar)"//...
+        );
+
+        try {
+            int options = Menu.readIntegerLine();
+
+            // TODO: retornar instancia da criatura selecionada
+
+        } catch (Exception e) {
+            System.out.println("Opção inválida!\n");
+            return selecionaCriatura();
+        }
+        throw new Error("Ops, Erro interno");
     }
 }
